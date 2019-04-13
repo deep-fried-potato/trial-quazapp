46f6668405f0a76caf67717c3d29f9163fc256c1
Kevin <kevinjohn1999@gmail.com>
2019-04-14 00:51:05 +0530

    Added API for Login

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -4,6 +4,7 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var config = require('../config/secret');
+let models = require("../models")
 
 module.exports = function(models){
     let router = express.Router();
@@ -43,10 +44,44 @@ module.exports = function(models){
             return res.status(401).send({auth:false,message:'No token provided'});
     
         jwt.verify(token,config.secret,(err,decoded) =>{ 
-            if(err)
+            if(err){
+                console.log(err);
                 return res.status(500).send({auth:false,message:'Failed to authenticate token'});
+            }
+                
             
-            res.status(200).send(decoded);
+                // User.findById(decoded.id, function (err, user) {
+                //     if (err) return res.status(500).send("There was a problem finding the user.");
+                //     if (!user) return res.status(404).send("No user found.");
+                    
+                //     res.status(200).send(user);
+                // });
+                console.log(decoded);
+                models.sequelize.query(`SELECT "userid","username","email" FROM "Users" where "userid" = ${ decoded.id }`).then(([result,metadata])=>{
+                    if(result.length == 0)
+                        return res.status(404).send("No user found.");
+                    res.status(200).send(result);
+                }).catch((err)=>{
+                    console.log(err);
+                    return res.status(500).send("There was a problem finding the user.");
+                })
+        })
+        
+    })
+
+    router.post('/login',(req,res) => {
+        verifyUser(req.body.email,req.body.password).then((userid)=>{
+            console.log("RETURNED VALUE =",userid);
+            if(!userid) 
+                return res.status(401).send({ auth: false, token: null });
+            var token = jwt.sign({ id:userid },config.secret,{ expiresIn: 86400 });
+            res.status(200).send({ auth:true, token: token});
+        }).catch((err)=>{
+            if(err == "No User found")
+            {
+                res.status(404).send(err);
+            }
+            res.status(500).send(err);
         })
         
     })
@@ -54,4 +89,20 @@ module.exports = function(models){
     return router
 }
 
+
+function verifyUser(email,password){
+    return new Promise((resolve,reject)=>{
+        models.sequelize.query(`SELECT * FROM "Users" where "email" = '${ email }'`).then(([result,metadata])=>{
+        if(result.length == 0)
+            reject("No User found");
+        var passwordIsValid = bcrypt.compareSync(password,result[0].password);
+        if(!passwordIsValid)
+            resolve(false);
+        resolve(result[0].userid)
+        }).catch((err)=>{
+            reject("There was a problem finding the user.");
+        })
+   
+    })
+}
 
